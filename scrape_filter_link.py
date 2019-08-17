77ca1eeb9dd08353cc0c04787be98ac04f4370d5
parthparikh <parthparikh1999p@gmail.com>
2019-08-17 19:05:17 +0530

    - methods added to fetch title and 'wikipedia summary' for bookmark's vocabulary

--- scrape_filter_link.py
+++ scrape_filter_link.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from string import punctuation
+import requests
 
 class Scrape_Filter():
 	def __init__(self):
@@ -14,6 +15,13 @@ class Scrape_Filter():
 		req.download()
 		soup = BeautifulSoup(req.html, 'html.parser')
 		return soup, req
+
+	def get_title(self, soup):
+		if soup.title.string.find('|') != -1:
+			title = soup.title.string.split('|')[0]
+			return title.strip()
+		else:
+			return soup.title.string
 
 	def get_keywords_and_description(self, soup):
 		desc = [soup.find_all('meta', attrs={'name':'og:description'}), 
@@ -41,12 +49,25 @@ class Scrape_Filter():
 
 		return vocabulary
 
+	def get_wikipedia(self, title):
+		API_BEFORE = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="
+		API_AFTER = "&limit=1&namespace=0&format=json"
+		req = requests.get(API_BEFORE+title+API_AFTER)
+		corpus = req.json()[2][0]
+		return corpus
+
 if __name__ == '__main__':
 	obj = Scrape_Filter()
-	soup, req = obj.get_bookmark_link('https://reuters.com')
+	soup, req = obj.get_bookmark_link('https://gmail.com')
 	req.parse()
+	title = obj.get_title(soup)
 	desc_keywords = obj.get_keywords_and_description(soup)
 	content = obj.filter_text(req.text)
 
+	print(title)
 	print(desc_keywords)
 	print(content)
+
+	if desc_keywords == None and content == list():
+		wiki = obj.get_wikipedia(title)
+		print(wiki)
