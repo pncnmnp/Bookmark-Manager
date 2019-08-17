b97c4bdb2932c470b3478867700f77f1a0da1339
parthparikh <parthparikh1999p@gmail.com>
2019-08-17 18:01:03 +0530

    - updated 'get_corpus' to organize corpuses according to 'layout.json' - fetched corpuses of commonly used categories in './corpus/'

--- get_corpus.py
+++ get_corpus.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 API_BEFORE = "https://en.wikipedia.org/w/api.php?format=json&redirects=1&titles="
 API_AFTER = "&action=query&prop=extracts&explaintext&origin=*&limit=1"
@@ -7,8 +9,22 @@ def fetch(name):
 	req = requests.get(API_BEFORE + name + API_AFTER)
 	page_id = list(req.json()['query']['pages'].keys())[0]
 
-	corpus = req.json()['query']['pages'][page_id]['extract']
+	corpus = {"text": req.json()['query']['pages'][page_id]['extract']}
 	return corpus
 
 if __name__ == '__main__':
-	fetch('sports')
+	# This automation should be started from the '/' dir of the project.
+	DIR_LOC = './corpus/'
+	CORPUSES = './corpus/layout.json'
+	layouts = json.load(open(CORPUSES))
+
+	for category in list(layouts.keys()):
+		if os.path.exists(DIR_LOC+category) == False:
+			os.mkdir(DIR_LOC+category)
+
+		for title in layouts[category]:
+			TARGET_DIR = DIR_LOC+category+'/'+title+'.json'
+			if os.path.exists(TARGET_DIR) == False:
+				corpus = fetch(title)
+				with open(TARGET_DIR, 'w') as fp:
+					json.dump(corpus, fp)
